e12b1a782fb6dc30c1ae0c88b8537feb5e974fde
finn <32391033+deftkHD@users.noreply.github.com>
2019-11-26 15:15:55 +0100

    implemented going back inside webview

--- app/src/main/java/com/elvers/gereon/stgnewsapp1/ArticleActivity.java
+++ app/src/main/java/com/elvers/gereon/stgnewsapp1/ArticleActivity.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.NavUtils;
@@ -261,6 +262,13 @@ public class ArticleActivity extends AppCompatActivity {
         if (isComments) {
             showArticle();
         } else {
+            Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.article_frame);
+            if(fragment instanceof ArticleFragment) {
+                if(((ArticleFragment) fragment).webView.canGoBack()) { // kotlin smart cast would be nice here...
+                    ((ArticleFragment) fragment).webView.goBack();
+                    return;
+                }
+            }
             super.onBackPressed();
         }
     }
